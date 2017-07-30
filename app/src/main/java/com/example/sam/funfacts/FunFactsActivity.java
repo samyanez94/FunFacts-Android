44e88f772f350cc072df4f0dd8cbb86b160f1334
Samuel Yanez <samuelyanez94@gmail.com>
2017-07-30 11:56:29 -0700

    Added logic to randomly select a new fact

--- app/src/main/java/com/example/sam/funfacts/FunFactsActivity.java
+++ app/src/main/java/com/example/sam/funfacts/FunFactsActivity.java
@@ -2,12 +2,47 @@ package com.example.sam.funfacts;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import java.util.Random;
 
 public class FunFactsActivity extends AppCompatActivity {
+
+    private TextView mFactTextView;
+    private Button mShowFactButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fun_facts);
+
+        // Assign the Views from the layout file to the corresponding variables
+        mFactTextView = (TextView) findViewById(R.id.factTextView);
+        mShowFactButton = (Button) findViewById(R.id.showFactButton);
+
+        final String[] facts = {
+                "Ants stretch when they wake up in the morning.",
+                "Ostriches can run faster than horses.",
+                "Olympic gold medals are actually made mostly of silver.",
+                "You are born with 300 bones; by the time you are an adult you will have 206.",
+                "It takes about 8 minutes for light from the Sun to reach Earth.",
+                "Some bamboo plants can grow almost a meter in just one day.",
+                "The state of Florida is bigger than England.",
+                "Some penguins can leap 2-3 meters out of the water.",
+                "On average, it takes 66 days to form a new habit.",
+                "Mammoths still walked the earth when the Great Pyramid was being built." };
+
+        // The button was clicked, so update the fact TextView with a new fact
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Create a random number to pick a fact
+                Random randomGenerator = new Random();
+                // Update the screen with our dynamic fact
+                mFactTextView.setText(facts[randomGenerator.nextInt(facts.length)]);
+            }
+        };
+        mShowFactButton.setOnClickListener(listener);
     }
 }
